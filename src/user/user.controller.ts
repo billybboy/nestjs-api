604f663e1fff6c5672fe379d0d97ce711e72a8e1
bboy <bboy@dailyview.tw>
2024-06-19 22:48:43 +0800

    initial commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { User } from '@prisma/client';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+    @Get('me')
+    getMe(@GetUser() user: User, @GetUser('email') email: string) {
+        console.log({email});
+        return user;
+    }
+}
